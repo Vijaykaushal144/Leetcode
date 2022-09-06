5c1988354ce2790ac10cd54457ebb0dd7df0b685
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2022-09-06 10:12:39 +0530

    Time: 0 ms (100.00%), Space: 42 MB (91.50%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -10,7 +10,7 @@ class Solution {
             {
             st=mid+1;
             }
-            else if(nums[mid]>target)
+            else 
             {
                 end=mid-1;
             }
