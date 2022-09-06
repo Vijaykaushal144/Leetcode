d39ee2aa09f6f6ee69325a6d0fceaa8674834cf3
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2022-09-06 10:21:12 +0530

    Time: 121 ms (11.61%), Space: 44.8 MB (69.92%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         {
             for(int j=i+1;j<nums.length;j++)
             {
-                if(nums[j]==target-nums[i]) return new int[] {i,j};
+                if(nums[j]+nums[i]==target) return new int[] {i,j};
             }
         }
         return null;
