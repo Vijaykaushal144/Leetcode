9aab00fcbd5161b74107a04d2cd5c8481983af28
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2022-09-18 02:27:01 +0530

    Time: 67 ms (22.21%), Space: 119.6 MB (5.11%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,7 +1,55 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-       Arrays.sort(nums); 
+        int st=0;
+        int end=nums.length-1;
+        mergeSort(nums,st,end);
         return nums;
     }
-    
+    public int[] mergeSort(int arr[], int left , int right){
+        if (left < right){
+            int mid = (left+right)/2;
+            mergeSort(arr,left,mid);
+            mergeSort(arr,mid+1,right);
+            merge(arr,left,mid,right);
+        }
+        return arr;
+    }
+    public int[] merge(int a[],int low, int mid, int end){
+        int n1=mid-low+1;
+       int n2=end-mid;
+       int[] l1 =new int[n1];
+       int[] l2 =new int[n2];
+       for(int i=0;i<n1;i++)
+       {
+           l1[i]=a[low+i];
+
+       }
+       for (int j=0;j<n2;j++)
+       {
+           l2[j]=a[mid+1+j];
+       }
+
+       int i=0,j=0,k=low;
+       while(i<n1 && j<n2)
+       {
+           if(l1[i]<l2[j])
+           {
+               a[k++]=l1[i++];
+           }
+           else {
+               a[k++] = l2[j++];
+           }
+
+       }
+       while(i<n1)
+       {
+           a[k++]=l1[i++];
+       }
+       while(j<n2)
+       {
+           a[k++] = l2[j++];
+       }
+        
+        return  a;
+    }
 }
