a1f33827d73ba2fe7a0faa3121d2a2914513efb2
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2022-09-28 00:52:14 +0530

    Time: 2 ms (84.97%), Space: 49.2 MB (67.42%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,19 +1,35 @@
 class Solution {
     public int jump(int[] nums) {
-        int n=nums.length;
-       int dp[]=new int[n];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[n-1]=0;
+//         int n=nums.length;
+//        int dp[]=new int[n];
+//         Arrays.fill(dp,Integer.MAX_VALUE);
+//         dp[n-1]=0;
        
-        for(int i=n-2;i>=0;i--)
-        {
-             int min=Integer.MAX_VALUE;
-           for(int j=i+1;j<=Math.min(n-1,i+nums[i]);j++)
-               {min=Math.min(min,dp[j]);}
+//         for(int i=n-2;i>=0;i--)
+//         {
+//              int min=Integer.MAX_VALUE;
+//            for(int j=i+1;j<=Math.min(n-1,i+nums[i]);j++)
+//                {min=Math.min(min,dp[j]);}
             
-            if(min!=Integer.MAX_VALUE) {dp[i]=min+1;}
-        }
+//             if(min!=Integer.MAX_VALUE) {dp[i]=min+1;}
+//         }
       
-        return dp[0];
+//         return dp[0];
+        int curReach=0;
+        int curMax=0;
+        int jump=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(i+nums[i]>curMax)
+            {
+                curMax=i+nums[i];
+            }
+            if(i==curReach)
+            {
+                jump++;
+                curReach=curMax;
+            }
+        }
+        return jump;
     }
 }
