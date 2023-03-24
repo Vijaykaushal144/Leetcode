405c1fbfde773cc55a53a9877fb45563a9af1ac1
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2023-03-24 19:26:54 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (14.73%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,18 +13,46 @@
  *     }
  * }
  */
+
+//using iteration.
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer>ls= new ArrayList<Integer>();
-        dfs(root,ls);
-        return ls;
-    }
-    public void dfs(TreeNode root,List<Integer> ls)
-    {
-        if(root==null) return ;
-        
-        dfs(root.left,ls);
-        ls.add(root.val);
-        dfs(root.right,ls);
+        ArrayList < Integer > inOrder = new ArrayList < > ();
+        Stack <TreeNode> s = new Stack <> ();
+        while (true) {
+            if (root != null) {
+                s.push(root);
+                 //insert value in to stack and move left of tree
+                root = root.left;
+            } else {
+                if (s.isEmpty()) break;
+                root = s.pop();
+                inOrder.add(root.val);
+                 //insert value in to stack and move  of tree
+                root = root.right;
+            }
+        }
+        return inOrder;
+       
     }
 }
+
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         // List<Integer>ls= new ArrayList<Integer>();
+//         // dfs(root,ls);
+//         // return ls;
+//     }
+// //     public void dfs(TreeNode root,List<Integer> ls)
+// //     {
+// //         if(root==null) return ;
+        
+// //         dfs(root.left,ls);
+// //         ls.add(root.val);
+// //         dfs(root.right,ls);
+// //     }
+// }
+
+
+
