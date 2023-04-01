5393c43120e5ba7a786a1ca4cb8be1d92b2c0047
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2023-04-01 22:37:17 +0530

    Time: 13 ms (26.95%), Space: 47.9 MB (14.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,42 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         // Arrays.sort(nums);
+//         // return nums[nums.length/2];
+        
+//         int a=nums[0];
+//         int count=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]==a)
+//             {
+//                 count++;
+//             }
+//             else
+//             {
+//                 count--;
+//             }
+//              if(count<=0)
+//         {
+//             a=nums[i];
+//             count=1;
+//         }
+//         }
+       
+//         return a;
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
-        // Arrays.sort(nums);
-        // return nums[nums.length/2];
-        
-        int a=nums[0];
-        int count=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==a)
-            {
-                count++;
-            }
-            else
-            {
-                count--;
-            }
-             if(count<=0)
-        {
-            a=nums[i];
-            count=1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i])>nums.length/2){
+                return nums[i];
+            }
         }
-       
-        return a;
+        return 0;
     }
 }
