e6fb73336f456f07e6ed76c6901cd27afb222887
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2023-04-04 10:46:19 +0530

    Time: 1 ms (95.22%), Space: 42.9 MB (37.73%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> sublist= new ArrayList<>();
+        boolean []visit= new boolean[nums.length];
+        recursion(res, sublist,nums,visit);
+        
+        return res;   }
+    
+    
+    public void recursion(List<List<Integer>> list, List<Integer> sublist, int nums[], boolean[] visited)
+    {
+        if(sublist.size()==nums.length)
+        {
+            list.add(new ArrayList(sublist));
+            return;
+        }
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(visited[i]==true)
+                continue;
+            sublist.add(nums[i]);
+            visited[i]=true;
+            recursion(list,sublist,nums,visited);
+            sublist.remove(sublist.size()-1);
+            visited[i]=false;
+            
+           
+        }
+    }
+}
