0edbbbc9130ef436816ab1a17310692f76037207
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2023-04-11 10:28:50 +0530

    Time: 0 ms (100.00%), Space: 50 MB (13.84%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,14 +1,35 @@
 class Solution {
     public int hIndex(int[] citations) {
         
-        int n=citations.length;
-        for( int i=0;i<n;i++)
+         int n=citations.length;
+        // for( int i=0;i<n;i++)
+        // {
+        //     if(n-i<=citations[i])
+        //     {
+        //         return n-i;
+        //     }
+        // }
+        // return 0;
+        
+        //implement binary search
+        
+        int st=0;
+        int ans=0;
+        int end=citations.length;
+        while(st<end)
         {
-            if(n-i<=citations[i])
+            int mid=st+(end-st)/2;
+         if((n-mid)<=citations[mid])
             {
-                return n-i;
+                ans=Math.max(n-mid,ans);
+                     end=mid;
             }
+            
+            else
+                st=mid+1;
         }
-        return 0;
+        
+        
+        return ans;
     }
 }
