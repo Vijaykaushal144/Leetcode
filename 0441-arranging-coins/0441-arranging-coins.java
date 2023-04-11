c713556efab8505e12ad5ff96119022778f877db
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2023-04-11 10:47:12 +0530

    Time: 1 ms (100.00%), Space: 40.3 MB (20.06%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,20 +1,22 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long l=0,r=n;
-        long mid,k;
-        while(l<=r){
-            mid=l+(r-l)/2;
-            k=mid*(mid+1)/2;
-            if(k==n){
-                return (int)mid;
-            }
-            else if(k<n){
-                l=mid+1;
-            }
-            else{
-                r=mid-1;
-            }
-        }
-        return (int)r;
+        // long l=0,r=n;
+        // long mid,k;
+        // while(l<=r){
+        //     mid=l+(r-l)/2;
+        //     k=mid*(mid+1)/2;
+        //     if(k==n){
+        //         return (int)mid;
+        //     }
+        //     else if(k<n){
+        //         l=mid+1;
+        //     }
+        //     else{
+        //         r=mid-1;
+        //     }
+        // }
+        // return (int)r;
+        
+        return (int) (Math.sqrt(2*(long)n+0.25)-0.5);
     }
 }
