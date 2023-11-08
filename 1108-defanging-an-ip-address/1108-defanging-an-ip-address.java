b2f089478fef60b9b10f6a25257ad2ed2572a7b4
Vijaykaushal144 <88468905+Vijaykaushal144@users.noreply.github.com>
2023-11-08 09:40:03 +0530

    Time: 6 ms (5.73%), Space: 40.2 MB (82.79%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -3,20 +3,30 @@ class Solution {
        
        // return address.replace(".","[.]");
         
-        StringBuffer res= new StringBuffer();
-        int idx=0;
+//         StringBuffer res= new StringBuffer();
+//         int idx=0;
        
-          while(idx<address.length())
-         {
-              if(address.charAt(idx)=='.')
-            {
-                res.append("[.]");
-            }
-              else res.append(address.charAt(idx));
+//           while(idx<address.length())
+//          {
+//               if(address.charAt(idx)=='.')
+//             {
+//                 res.append("[.]");
+//             }
+//               else res.append(address.charAt(idx));
            
+//             idx++;
+//         }
+//        return res.toString();
+        
+        String res="";
+        int idx=0;
+        while(idx<address.length())
+        {
+            if(address.charAt(idx)=='.') res+="[.]";
+            else res+=address.charAt(idx);
             idx++;
         }
-       return res.toString();
+        return res;
 
     }
 }
